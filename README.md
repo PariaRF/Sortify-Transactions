1328b80ddc5c1f24a6ad973fd7588ee9a7d1955d
PariaRF <PariaRF>
2025-01-08 17:06:50 +0100

    final commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center" id="title">Sortify Transactions</h1>
+
+<p id="description">This mini project features a responsive table of transactions that allows users to sort entries in ascending or descending order by date or price. The application dynamically updates the URL to reflect the current sorting state and automatically cleans the URL of any query parameters upon refresh ensuring a seamless user experience.</p>
+
+<h2>Project Screenshots:</h2>
+
+<img src="https://i.postimg.cc/G2Tf6fYy/js-final-task.png" alt="project-screenshot" width="800" height="400/">
+
+<h2>🧐 Features</h2>
+
+Here're some of the project's best features:
+
+- Responsive Design: Fully responsive layout for optimal viewing on all devices.
+- Sorting Functionality: Users can sort transactions by date or price in both ascending and descending order.
+- Dynamic URL Management: Automatically updates the URL to reflect the current sorting state and cleans query parameters on page refresh.
+- Axios Configuration: Utilizes Axios for making API calls simplifying HTTP requests and responses.
+- Toast Notifications: Displays user-friendly toast notifications using React Toastify based on the API response messages enhancing user feedback and experience.
+
+<h2>🛠️ Installation Steps:</h2>
+
+<p>1. clone code.</p>
+
+<p>2. npm install.</p>
+
+<p>3. npm run server.</p>
+
+<p>4. To view the project please open the index.html file using a live server.</p>
---
+++ src/js/index.js
@@ -0,0 +1,90 @@
+import TabelBody from "./TabelBody.js";
+import getAllTransaction from "./services/getAllTransaction.js";
+import searchAndSort from "./services/searchAndSort.js";
+import sortTransactions from "./services/sortTransactions.js";
+
+// GLOBAL VARIABLE
+let theElement = "price";
+let sortOrder = "asc";
+let filteredTransaction = [];
+let url;
+
+// SELECT
+const transactionLoadBtn = document.querySelector(".transaction__load-btn");
+const transactionList = document.querySelector(".transaction__list");
+const transactionInitialLoad = document.querySelector(
+  ".transaction__initial-load"
+);
+const priceTh = document.querySelector(".price-th");
+const dateTh = document.querySelector(".date-th");
+const listSearchInput = document.querySelector(".list__search__input");
+
+// EVENTS
+document.addEventListener("DOMContentLoaded", cleanUrl);
+
+transactionLoadBtn.addEventListener("click", async () => {
+  const allTransaction = await getAllTransaction();
+
+  TabelBody(allTransaction);
+
+  transactionInitialLoad.style.display = "none";
+  transactionList.style.display = "block";
+});
+
+priceTh.addEventListener("click", (e) => {
+  theElement = "price";
+  handleSortClick(priceTh, "price");
+});
+
+dateTh.addEventListener("click", () => {
+  theElement = "date";
+  handleSortClick(dateTh, "date");
+});
+
+listSearchInput.addEventListener("input", async (e) => {
+  handleSearchTerm(e);
+});
+
+// FUNCTIONS
+function cleanUrl() {
+  url = new URL(window.location.href);
+  url.searchParams.delete("price");
+  url.searchParams.delete("date");
+  window.history.pushState({}, "", url);
+}
+
+async function handleSortClick(thElement, sortKey) {
+  const svgElement = thElement.children[0];
+  svgElement.classList.toggle("chevron--rotate");
+
+  sortOrder = svgElement.classList.contains("chevron--rotate") ? "asc" : "desc";
+
+  cleanUrl();
+
+  url.searchParams.set(sortKey, sortOrder);
+  window.history.pushState({}, "", url);
+
+  if (filteredTransaction.length > 0) {
+    sortExistData(filteredTransaction, sortOrder);
+  } else {
+    const sortedData = await sortTransactions(sortKey, sortOrder);
+    TabelBody(sortedData);
+  }
+}
+
+async function handleSearchTerm(e) {
+  filteredTransaction = await searchAndSort(
+    e.target.value,
+    theElement,
+    sortOrder
+  );
+  TabelBody(filteredTransaction);
+}
+
+function sortExistData(filteredTransaction, sortOrder) {
+  const sortedExistDate = filteredTransaction.sort((a, b) => {
+    if (sortOrder === "asc") return new Date(a.date) - new Date(b.date);
+    return new Date(b.date) - new Date(a.date);
+  });
+  TabelBody(sortedExistDate);
+}
